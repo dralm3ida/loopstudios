4585182bb8cf86e0446ab3c87e4ff7c670e3ac66
r0drig <andralmeida0@gmail.com>
2021-06-11 18:55:52 +0100

    Mobile view part 2

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ var body = document.querySelector("body");
 var headerLinks = document.querySelectorAll(".header-mobile-links,.header-desktop-links");
 
 btnMenu.addEventListener('click', event => {
-   body.classList.toggle('open');
+   body.classList.toggle('mobile-menu');
    headerLinks.forEach(linkMenu =>{
       linkMenu.classList.toggle('hide');
    })
